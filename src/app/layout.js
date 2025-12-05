0090a8f4add5ecb99040ca5db0a333e2e9fd7ee2
marzi <marzilightsep24@gmail.com>
2025-12-05 16:53:53 +0330

    change font and add background image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,16 @@
 import "./globals.css";
+import { Geist } from 'next/font/google'
+import Header from "@/components/Header";
+ 
+const geist = Geist({
+  subsets: ['latin'],
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fa">
+    <html lang="en" className={geist.className}>
       <body>
+        <Header/>
         {children}
       </body>
     </html>
